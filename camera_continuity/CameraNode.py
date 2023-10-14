30f3dff21871dfb64e13a9d9aec61173b5f753a9
Sahil <spatel3038@gatech.edu>
2023-10-14 13:56:36 -0400

    Initial splitting code

--- camera_continuity/CameraNode.py
+++ camera_continuity/CameraNode.py
@@ -9,7 +9,14 @@ class CameraNode:
     def get_neighbor(self):
         return self.neighbors
     
+    def get_video_feed(self, frame):
+        #return path to directory containing video feed
+        return "video-data/raw-video/camera" + str(self.value) + ".mp4"
+    
     def get_frame(self, frame):
         #return path to directory containing video feed
         return "video-data/frames/camera" + str(self.value) + "/frame" + str(frame) + ".jpg"
+    def get_frame_path(self):
+        #return path to directory containing video feed
+        return "video-data/frames/camera" + str(self.value)
             
--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,8 @@
-import sys
+import sys, os
+sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
 from time import sleep
+from camera_continuity.CameraNode import CameraNode
+from frame_split import split_video
 
 #create the node of the cameras to monitor, initially all of them
 
@@ -11,32 +14,35 @@ frame = 0
 
 def init_cameras():
     camera_a = CameraNode(1)
-    camera_b = CameraNode(2)
-    camera_c = CameraNode(3)
-    camera_d = CameraNode(4)
-    camera_e = CameraNode(5)
+    # camera_b = CameraNode(2)
+    # camera_c = CameraNode(3)
+    # camera_d = CameraNode(4)
+    # camera_e = CameraNode(5)
 
     monitored_cameras.append(camera_a) 
-    monitored_cameras.append(camera_b) 
-    monitored_cameras.append(camera_c) 
-    monitored_cameras.append(camera_d) 
-    monitored_cameras.append(camera_e) 
+    # monitored_cameras.append(camera_b) 
+    # monitored_cameras.append(camera_c) 
+    # monitored_cameras.append(camera_d) 
+    # monitored_cameras.append(camera_e) 
 
                              
 
 if __name__ == "__main__":
     init_cameras()
-    while True:
-        flagged_cameras = []
-        for camera in monitored_cameras:
-            print(camera.get_video_feed(frame))
-            # implement gun detection here
-            is_gun_detected = False
-            if is_gun_detected:
-                flagged_cameras.append(camera)
+    print(monitored_cameras[0].get_video_feed(0))
+    split_video.split(monitored_cameras[0].get_video_feed(0), monitored_cameras[0].get_frame_path())
+
+    # while True:
+    #     flagged_cameras = []
+    #     for camera in monitored_cameras:
+    #         print(camera.get_video_feed(frame))
+    #         # implement gun detection here
+    #         is_gun_detected = False
+    #         if is_gun_detected:
+    #             flagged_cameras.append(camera)
         
 
-        sleep(1)
-        frame += 1
+    #     sleep(1)
+    #     frame += 1
 
 

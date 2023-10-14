0b243c7171c31af1abe7f3dcc03f1c9e26ba1d3f
Mrinal Jain <2mrinaljain@gmail.com>
2023-10-14 14:18:10 -0400

    extract bbox coords

--- person-detection/detect_person.py
+++ person-detection/detect_person.py
@@ -1,4 +1,11 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
-results = model(source="/Users/ashwathkarunakaram/Documents/ShooterTracker/fire-and-gun-detection/screenshots/person1.jpg", show=True, conf=0.4, save=True)
+results = model(source="people.jpeg", show=False, conf=0.4, save=True)[0].boxes
+bboxes = []
+for i in range(len(results.cls)):
+    if results.cls[i] != 0:
+        continue
+    bboxes.append(results.data[i].cpu().tolist())
+
+print(bboxes)

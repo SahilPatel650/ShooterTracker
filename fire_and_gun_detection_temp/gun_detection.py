91e8c38e37b9702e0cf1693b4132fbb1dc58c8c3
Mrinal Jain <2mrinaljain@gmail.com>
2023-10-14 21:39:51 -0400

    shooter identification

---
+++ fire_and_gun_detection_temp/gun_detection.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np 
+import argparse
+
+#Load yolo
+def load_yolo():
+	net = cv2.dnn.readNet("fire_and_gun_detection/yolov3.weights", "fire_and_gun_detection/yolov3.cfg")
+	classes = []
+	with open("fire_and_gun_detection/obj.names", "r") as f:
+		classes = [line.strip() for line in f.readlines()]
+
+	layers_names = net.getLayerNames()
+	output_layers = [layers_names[i-1] for i in net.getUnconnectedOutLayers()]
+	colors = np.random.uniform(0, 255, size=(len(classes), 3))
+	return net, classes, colors, output_layers
+
+def load_image(img_path):
+	# image loading
+	img = cv2.imread(img_path)
+	img = cv2.resize(img, None, fx=0.4, fy=0.4)
+	height, width, channels = img.shape
+	return img, height, width, channels
+
+def display_blob(blob):
+	'''
+		Three images each for RED, GREEN, BLUE channel
+	'''
+	for b in blob:
+		for n, imgb in enumerate(b):
+			cv2.imshow(str(n), imgb)
+
+def detect_objects(img, net, outputLayers):			
+	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+	net.setInput(blob)
+	outputs = net.forward(outputLayers)
+	return blob, outputs
+
+def get_box_dimensions(outputs, height, width):
+	boxes = []
+	confs = []
+	class_ids = []
+	for output in outputs:
+		for detect in output:
+			scores = detect[5:]
+			class_id = np.argmax(scores)
+			conf = scores[class_id]
+			if conf > 0.3:
+				center_x = int(detect[0] * width)
+				center_y = int(detect[1] * height)
+				w = int(detect[2] * width)
+				h = int(detect[3] * height)
+				x = int(center_x - w/2)
+				y = int(center_y - h / 2)
+				boxes.append([x, y, w, h])
+				confs.append(float(conf))
+				class_ids.append(class_id)
+	return boxes, confs, class_ids
+			
+def draw_labels(boxes, confs, colors, class_ids, classes, img): 
+	indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+	font = cv2.FONT_HERSHEY_PLAIN
+	for i in range(len(boxes)):
+		if i in indexes:
+			x, y, w, h = boxes[i]
+			label = str(classes[class_ids[i]])
+			color = (0,0,255)
+			cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+			cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
+	img=cv2.resize(img, (800,600))
+	#cv2.imshow("Image", img)
+
+def draw_person_box(box, img_path):
+    x1, y1, x2, y2 = [int(i*0.4) for i in box[:4]]
+    img = cv2.imread(img_path)
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0,0,255), 2)
+    image_write(img_path, img)
+
+def image_write(img_path, img):
+	path = img_path.split('/')
+	path[1] = 'rendered-frames'
+	new_path = '/'.join(path)
+	print(new_path)
+	cv2.imwrite(new_path, img)
+
+def image_detect(img_path, loaded_model): 
+	model, classes, colors, output_layers = loaded_model
+	image, height, width, channels = load_image(img_path)
+	blob, outputs = detect_objects(image, model, output_layers)
+	boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+	draw_labels(boxes, confs, colors, class_ids, classes, image)
+	image_write(img_path, image)
+	
+	return boxes, confs, class_ids

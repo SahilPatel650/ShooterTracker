0925bc283973334d581125fd8e0967431bd62446
Ashwath Karunakaram <ashwath.karunakaram245@gmail.com>
2023-10-15 03:32:03 -0400

    rendering frames

--- entrypoint.py
+++ entrypoint.py
@@ -1,16 +1,17 @@
 import sys, os
 sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
-from time import sleep
+
+import mapdraw
+from PIL import Image
 from camera_continuity.CameraNode import CameraNode
 from frame_split import split_video
-from frame_split import merge_video
 
 from fire_and_gun_detection import gun_detection
 from person_detection import detect_person
 from person_detection import id_shooter
 from shooter_description import desc
 
-#create the node of the cameras to monitor, initially all of them
+# create the node of the cameras to monitor, initially all of them
 
 NUM_CAMERAS = 4
 num_frames = [0, 0, 0, 0]
@@ -19,23 +20,31 @@ last_detected_camera: CameraNode = None
 all_cameras = []
 frame = 0
 
+camera_coords = [
+    (318, 411), (0, 520), (0, 90), (385, 0)
+]
+
+angles = [
+    (-180, 0), (-90, 90), (-90, 90), (0, -180)
+]
+
+
 def init_cameras():
-    camera_1 = CameraNode(1, [1,2])
-    camera_2 = CameraNode(2, [1,2,3])
-    camera_3 = CameraNode(3, [2,3,4])
-    camera_4 = CameraNode(4, [3,4])
+    camera_1 = CameraNode(1, [1, 2])
+    camera_2 = CameraNode(2, [1, 2, 3])
+    camera_3 = CameraNode(3, [2, 3, 4])
+    camera_4 = CameraNode(4, [3, 4])
 
-    monitored_cameras.append(camera_1) 
-    monitored_cameras.append(camera_2) 
-    monitored_cameras.append(camera_3) 
-    monitored_cameras.append(camera_4) 
+    monitored_cameras.append(camera_1)
+    monitored_cameras.append(camera_2)
+    monitored_cameras.append(camera_3)
+    monitored_cameras.append(camera_4)
 
-    all_cameras.append(camera_1) 
-    all_cameras.append(camera_2) 
-    all_cameras.append(camera_3) 
-    all_cameras.append(camera_4) 
-    
-    
+    all_cameras.append(camera_1)
+    all_cameras.append(camera_2)
+    all_cameras.append(camera_3)
+    all_cameras.append(camera_4)
+
 
 def init_models():
     gun_model = gun_detection.load_yolo()
@@ -43,39 +52,55 @@ def init_models():
     desc_model = desc.initialize_model()
     return gun_model, people_model, desc_model
 
+
 if __name__ == "__main__":
     init_cameras()
 
     gun_model, people_model, (processor, vqa_model) = init_models()
     description = ""
     for camera_idx in range(len(all_cameras)):
-        num_frames[camera_idx] = split_video.split(all_cameras[camera_idx].get_video_feed(), all_cameras[camera_idx].get_frame_path())
+        num_frames[camera_idx] = split_video.split(all_cameras[camera_idx].get_video_feed(),
+                                                   all_cameras[camera_idx].get_frame_path())
     common_frames = min(num_frames)
 
     for i in range(common_frames):
         new_monitored_cameras = []
+        # draw on map based on new_monitored_cameras
+        image = Image.open("video-data/rawmap.png")
+        if len(monitored_cameras) == 1:
+            image = mapdraw.color_semicircle(image, (255, 0, 0), camera_coords[monitored_cameras[0].value - 1][0],
+                                     camera_coords[monitored_cameras[0].value - 1][1],
+                                     angles[monitored_cameras[0].value - 1][0],
+                                     angles[monitored_cameras[0].value - 1][1])
+        else:
+            for item in monitored_cameras:
+                image = mapdraw.color_semicircle(image, (255, 255, 0), camera_coords[item.value - 1][0],
+                                         camera_coords[item.value - 1][1],
+                                         angles[item.value - 1][0],
+                                         angles[item.value - 1][1])
+        image.save("video-data/rendered-maps/frame" + str(i) + ".png")
+
         for camera_idx in range(len(monitored_cameras)):
 
             boxes, confs, class_ids = monitored_cameras[camera_idx].get_gun_bboxes(i, gun_model)
             people_boxes = monitored_cameras[camera_idx].get_people_bboxes(i, people_model)
 
-            if len(boxes) > 0 :
+            if len(boxes) > 0:
                 new_monitored_cameras = [monitored_cameras[camera_idx]]
                 last_detected_camera = all_cameras[camera_idx]
-                
 
             if len(people_boxes) == 0 or len(boxes) == 0:
                 continue
-            #add code to draw people boxes
+            # add code to draw people boxes
             shooter_id = id_shooter.id_shooter(people_boxes, boxes[0])
-            rendered_frame_path = monitored_cameras[camera_idx].get_rendered_frames()+"/frame"+str(i)+".jpg"
+            rendered_frame_path = monitored_cameras[camera_idx].get_rendered_frames() + "/frame" + str(i) + ".jpg"
             shooter_box = people_boxes[shooter_id][:4]
-            shooter_box = list(map(lambda x: int(x*0.4), shooter_box))
+            shooter_box = list(map(lambda x: int(x * 0.4), shooter_box))
             gun_detection.draw_person_box(shooter_box, rendered_frame_path)
             if not description:
-                description = desc.get_description(rendered_frame_path, shooter_box[:2], shooter_box[2:4], processor, vqa_model)
+                description = desc.get_description(rendered_frame_path, shooter_box[:2], shooter_box[2:4], processor,
+                                                   vqa_model)
             print(description)
-
 
         if new_monitored_cameras == []:
             if last_detected_camera == None:
@@ -83,13 +108,10 @@ if __name__ == "__main__":
             else:
                 neighbors = last_detected_camera.get_neighbors()
                 for neighbor in neighbors:
-                    new_monitored_cameras.append(all_cameras[neighbor-1])
-
+                    new_monitored_cameras.append(all_cameras[neighbor - 1])
 
         monitored_cameras = new_monitored_cameras
 
-            
-        
         # merge frames into video
         # merge_video.frames_to_video(monitored_cameras[camera_idx].get_rendered_frames(), monitored_cameras[camera_idx].get_render_video_path())
 
@@ -101,6 +123,6 @@ if __name__ == "__main__":
     #         is_gun_detected = False
     #         if is_gun_detected:
     #             flagged_cameras.append(camera)
-        
+
     #     sleep(1)
     #     frame += 1
---
+++ mapdraw.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageDraw
+
+def color_semicircle(image, color, center_x1, center_y1, startInt, endInt, savePath):
+    width, height = image.size
+    # Create a new image with the same size and a transparent background
+    semicircle_image = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(semicircle_image)
+    center_x = center_x1
+    center_y = center_y1
+    # Define the radius and color (red or yellow)
+    radius = 50  # Adjust the radius as needed
+
+    # Draw the semicircle
+    draw.pieslice(((center_x - radius, center_y - radius),
+                   (center_x + radius, center_y + radius)),
+                  startInt, endInt, fill=color)  # -90, 90 for C3
+    # Combine the original image and the semicircle image
+    result_image = Image.alpha_composite(image.convert('RGBA'), semicircle_image)
+
+    # Save or display the result image
+    return result_image
